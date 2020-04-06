d985fae0ba73d0a84a2aae0f17268433db6f3d34
Tairone Livinalli <tairone.livinali@softplan.com.br>
2020-04-06 09:13:56 -0300

    Initial commit

---
+++ src/app/models/User.js
@@ -0,0 +1,46 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        newEmail: Sequelize.VIRTUAL,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      { sequelize }
+    );
+
+    this.addHook('beforeSave', async (user) => {
+      if (!user.password) {
+        return;
+      }
+
+      user.password_hash = await bcrypt.hash(user.password, 8);
+    });
+
+    this.addHook('beforeSave', (user) => {
+      if (!user.newEmail) {
+        return;
+      }
+
+      user.email = user.newEmail;
+    });
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+export default User;
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'f15834ac2e9b29f15d54ddbc54bd012f',
+  expiresIn: '7d',
+};
