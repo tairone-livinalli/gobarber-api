eee267daaf5c93b8f29b76d4338b43bc914ee207
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2020-04-20 16:21:45 -0300

    Added cors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import 'express-async-errors';
 import Youch from 'youch';
 import path from 'path';
+import cors from 'cors';
 import * as Sentry from '@sentry/node';
 
 import routes from './routes';
@@ -22,6 +23,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
